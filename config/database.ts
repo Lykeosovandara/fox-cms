19dae21d0fb0320eb47ab783c72cd6319dd4f234
lykeosovandara <lykeosovandara@gmail.com>
2022-10-04 11:09:47 +0700

    first commit

---
+++ config/database.ts
@@ -0,0 +1,15 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'fox-db-do-user-8146940-0.b.db.ondigitalocean.com'),
+      port: env.int('DATABASE_PORT', 25060),
+      database: env('DATABASE_NAME', 'dev-fox'),
+      user: env('DATABASE_USERNAME', 'dev-fox'),
+      password: env('DATABASE_PASSWORD', 'AVNS_9IjxctEbwNQGeRmAidZ'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), 
+      },
+    },
+  },
+});
