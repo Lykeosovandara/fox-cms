8c06ce9b59c2de3965b51b0b68293965cee3c7a4
lykeosovandara <lykeosovandara@gmail.com>
2022-10-04 11:55:56 +0700

    change display name

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'fox-db-do-user-8146940-0.b.db.ondigitalocean.com'),
-      port: env.int('DATABASE_PORT', 25060),
-      database: env('DATABASE_NAME', 'dev-fox'),
-      user: env('DATABASE_USERNAME', 'dev-fox'),
-      password: env('DATABASE_PASSWORD', 'AVNS_9IjxctEbwNQGeRmAidZ'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), 
       },
