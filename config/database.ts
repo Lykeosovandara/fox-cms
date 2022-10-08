3ad8ff4e2ddb839a115160f251a9db62a78beec5
lykeosovandara <lykeosovandara@gmail.com>
2022-10-08 18:45:42 +0700

    change config

--- config/database.ts
+++ config/database.ts
@@ -1,13 +1,51 @@
-import { parse } from "pg-connection-string";
+const { parse } = require("pg-connection-string");
 
+module.exports = ({ env }) => {
+  if (process.env.NODE_ENV === "development") {
+    
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: "postgres",
+          port: 5432,
+          database: "strapi",
+          user: "strapi",
+          password: "strapi",
+          ssl: false,
+        },
+      },
+    };
+  } else {
+    const { host, port, database, user, password } = parse(
+      process.env.DATABASE_URL
+    );
 
-
-export default ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      url: env("DATABASE_URL_APP"),
-      // Why 
-    },
-  },
-});
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host,
+          port,
+          database,
+          user,
+          password,
+          ssl: {
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+          },
+        },
+        pool: {
+          min: 0,
+          max: 2,
+          acquireTimeoutMillis: 60000,
+          createTimeoutMillis: 60000,
+          destroyTimeoutMillis: 20000,
+          idleTimeoutMillis: 60000,
+          reapIntervalMillis: 1000,
+          createRetryIntervalMillis: 1000,
+        },
+      },
+      settings: { forceMigration: false },
+    };
+  }
+};
--- dist/config/database.js
+++ dist/config/database.js
@@ -1,11 +1,47 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ env }) => ({
-    connection: {
-        client: 'postgres',
-        connection: {
-            url: env("DATABASE_URL_APP"),
-            // Why 
-        },
-    },
-});
+const { parse } = require("pg-connection-string");
+module.exports = ({ env }) => {
+    if (process.env.NODE_ENV === "development") {
+        return {
+            connection: {
+                client: "postgres",
+                connection: {
+                    host: "postgres",
+                    port: 5432,
+                    database: "strapi",
+                    user: "strapi",
+                    password: "strapi",
+                    ssl: false,
+                },
+            },
+        };
+    }
+    else {
+        const { host, port, database, user, password } = parse(process.env.DATABASE_URL);
+        return {
+            connection: {
+                client: "postgres",
+                connection: {
+                    host,
+                    port,
+                    database,
+                    user,
+                    password,
+                    ssl: {
+                        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+                    },
+                },
+                pool: {
+                    min: 0,
+                    max: 2,
+                    acquireTimeoutMillis: 60000,
+                    createTimeoutMillis: 60000,
+                    destroyTimeoutMillis: 20000,
+                    idleTimeoutMillis: 60000,
+                    reapIntervalMillis: 1000,
+                    createRetryIntervalMillis: 1000,
+                },
+            },
+            settings: { forceMigration: false },
+        };
+    }
+};
