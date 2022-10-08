202201cc348628ae5b4aedcee2f45c4eb4aced73
lykeosovandara <lykeosovandara@gmail.com>
2022-10-08 19:55:04 +0700

    make it redeploy

--- config/database.ts
+++ config/database.ts
@@ -14,6 +14,7 @@ export default ({ env }) => ({
       password: pgS.parse(env('DATABASE_URL')).password,
       ssl: {
         rejectUnauthorized: false, 
+        // rejectUnauthorized: false, 
       },
     },
   },
