44f5b2c5193257240d3f9526f3da297901798f1c
lykeosovandara <lykeosovandara@gmail.com>
2022-10-08 19:44:21 +0700

    fk

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,30 +1,41 @@
-const { parse } = require("pg-connection-string");
-module.exports = ({ env }) => {
-    const { host, port, database, user, password } = parse(process.env.DATABASE_URL);
-    return {
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pgS = __importStar(require("pg-connection-string"));
+exports.default = ({ env }) => ({
+    connection: {
+        client: env('postgres'),
         connection: {
-            client: "postgres",
-            connection: {
-                host,
-                port,
-                database,
-                user,
-                password,
-                ssl: {
-                    rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-                },
-            },
-            pool: {
-                min: 0,
-                max: 2,
-                acquireTimeoutMillis: 60000,
-                createTimeoutMillis: 60000,
-                destroyTimeoutMillis: 20000,
-                idleTimeoutMillis: 60000,
-                reapIntervalMillis: 1000,
-                createRetryIntervalMillis: 1000,
+            host: pgS.parse(env('DATABASE_URL')).host,
+            port: pgS.parse(env.int('DATABASE_URL')).port,
+            database: pgS.parse(env('DATABASE_URL')).database,
+            user: pgS.parse(env('DATABASE_URL')).user,
+            password: pgS.parse(env('DATABASE_URL')).password,
+            ssl: {
+                rejectUnauthorized: false,
             },
         },
-        settings: { forceMigration: false },
-    };
-};
+    },
+});
