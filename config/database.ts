3bb3871fca0d9ccac45e6d9c84154fbb4474b438
lykeosovandara <lykeosovandara@gmail.com>
2022-10-08 16:46:12 +0700

    update config

--- config/database.ts
+++ config/database.ts
@@ -1,21 +1,20 @@
 import { parse } from "pg-connection-string";
 
 
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
+
 export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
       },
     },
+    debug: false,
   },
 });
