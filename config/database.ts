ff9ecd5a38a97f2bca87776c94cab21d76be1ed2
lykeosovandara <lykeosovandara@gmail.com>
2022-10-08 17:04:27 +0700

    update db config

--- config/database.ts
+++ config/database.ts
@@ -6,13 +6,13 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.PORT),
+      database: process.env.DATABASE,
+      user: process.env.USERNAME,
+      password: process.env.PASSWORD,
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        rejectUnauthorized: false,
       },
     },
     debug: false,
