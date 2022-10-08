2402b603dcb7e251b1e60486d16cc34c4282c961
lykeosovandara <lykeosovandara@gmail.com>
2022-10-08 19:02:19 +0700

    log host

--- config/database.ts
+++ config/database.ts
@@ -1,51 +1,30 @@
-const { parse } = require("pg-connection-string");
+import { parse } from "pg-connection-string";
+
+
 
 module.exports = ({ env }) => {
-  if (process.env.NODE_ENV === "development") {
-    
-    return {
-      connection: {
-        client: "postgres",
-        connection: {
-          host: "postgres",
-          port: 5432,
-          database: "strapi",
-          user: "strapi",
-          password: "strapi",
-          ssl: false,
-        },
-      },
-    };
-  } else {
-    const { host, port, database, user, password } = parse(
-      process.env.DATABASE_URL
-    );
 
-    return {
+  const { host, port, database, user, password } = parse(
+    process.env.DATABASE_URL
+  );
+
+  console.log("=====>", host);
+
+
+  return {
+    connection: {
+      client: "postgres",
       connection: {
-        client: "postgres",
-        connection: {
-          host,
-          port,
-          database,
-          user,
-          password,
-          ssl: {
-            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-          },
-        },
-        pool: {
-          min: 0,
-          max: 2,
-          acquireTimeoutMillis: 60000,
-          createTimeoutMillis: 60000,
-          destroyTimeoutMillis: 20000,
-          idleTimeoutMillis: 60000,
-          reapIntervalMillis: 1000,
-          createRetryIntervalMillis: 1000,
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
         },
       },
-      settings: { forceMigration: false },
-    };
-  }
+    },
+  };
+
 };
--- dist/config/database.js
+++ dist/config/database.js
@@ -1,47 +1,30 @@
 const { parse } = require("pg-connection-string");
 module.exports = ({ env }) => {
-    if (process.env.NODE_ENV === "development") {
-        return {
+    const { host, port, database, user, password } = parse(process.env.DATABASE_URL);
+    return {
+        connection: {
+            client: "postgres",
             connection: {
-                client: "postgres",
-                connection: {
-                    host: "postgres",
-                    port: 5432,
-                    database: "strapi",
-                    user: "strapi",
-                    password: "strapi",
-                    ssl: false,
+                host,
+                port,
+                database,
+                user,
+                password,
+                ssl: {
+                    rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
                 },
             },
-        };
-    }
-    else {
-        const { host, port, database, user, password } = parse(process.env.DATABASE_URL);
-        return {
-            connection: {
-                client: "postgres",
-                connection: {
-                    host,
-                    port,
-                    database,
-                    user,
-                    password,
-                    ssl: {
-                        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-                    },
-                },
-                pool: {
-                    min: 0,
-                    max: 2,
-                    acquireTimeoutMillis: 60000,
-                    createTimeoutMillis: 60000,
-                    destroyTimeoutMillis: 20000,
-                    idleTimeoutMillis: 60000,
-                    reapIntervalMillis: 1000,
-                    createRetryIntervalMillis: 1000,
-                },
+            pool: {
+                min: 0,
+                max: 2,
+                acquireTimeoutMillis: 60000,
+                createTimeoutMillis: 60000,
+                destroyTimeoutMillis: 20000,
+                idleTimeoutMillis: 60000,
+                reapIntervalMillis: 1000,
+                createRetryIntervalMillis: 1000,
             },
-            settings: { forceMigration: false },
-        };
-    }
+        },
+        settings: { forceMigration: false },
+    };
 };
