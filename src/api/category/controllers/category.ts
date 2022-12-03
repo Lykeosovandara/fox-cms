7a052d61d7f0f2178043ee854718b2ae19d99bd4
dara <lykeosovandara@gmail.com>
2022-12-03 17:19:53 +0700

    update category image

--- src/api/category/controllers/category.ts
+++ src/api/category/controllers/category.ts
@@ -4,4 +4,28 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::category.category');
+
+
+export default factories.createCoreController('api::category.category', ({ strapi: Strapi }) => ({
+    async find(ctx) {
+
+
+        const data = await strapi.entityService.findMany('api::category.category', {
+            populate: { image: true },
+            filters: {
+                image: {
+                    id: {
+                        $notNull: true
+                    },
+                }
+            },
+        })
+
+
+        return { data };
+    },
+
+
+
+
+}));
