5c7c3929329ca31090f85eab281b5ddbe67f7379
dara <lykeosovandara@gmail.com>
2022-12-04 13:19:30 +0700

    require field category image

--- dist/src/api/category/controllers/category.js
+++ dist/src/api/category/controllers/category.js
@@ -4,4 +4,18 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::category.category');
+exports.default = strapi_1.factories.createCoreController('api::category.category', ({ strapi: Strapi }) => ({
+    async find(ctx) {
+        const data = await strapi.entityService.findMany('api::category.category', {
+            populate: { image: true },
+            filters: {
+                image: {
+                    id: {
+                        $gte: 1
+                    },
+                }
+            },
+        });
+        return { data };
+    },
+}));
--- src/api/category/controllers/category.ts
+++ src/api/category/controllers/category.ts
@@ -8,24 +8,19 @@ import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::category.category', ({ strapi: Strapi }) => ({
     async find(ctx) {
-
-
         const data = await strapi.entityService.findMany('api::category.category', {
             populate: { image: true },
             filters: {
                 image: {
                     id: {
-                        $notNull: true
+                        $gte: 1
                     },
                 }
             },
         })
 
-
         return { data };
     },
-
-
 
 
 }));
