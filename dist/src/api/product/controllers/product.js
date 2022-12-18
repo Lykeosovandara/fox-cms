64c65a441203a29e31747e518068466847e24038
dara <lykeosovandara@gmail.com>
2022-12-18 14:28:27 +0700

    update new data

--- dist/src/api/product/controllers/product.js
+++ dist/src/api/product/controllers/product.js
@@ -5,11 +5,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
 exports.default = strapi_1.factories.createCoreController('api::product.product', ({ strapi }) => ({
-    // Method 1: Creating an entirely custom action
-    async exampleAction(ctx) {
-        return {
-            data: "Sucess"
-        };
+    async findOne(ctx) {
+        const orderid = ctx.params.id;
+        const data = await strapi.entityService.findOne('api::product.product', orderid, {
+            populate: ["images", "varients", "brands"],
+        });
+        const varients = data.varients;
+        let promises = [];
+        varients.forEach(({ id }) => {
+            promises.push(strapi.entityService.findOne('api::varient.varient', id, {
+                populate: ["images"],
+            }));
+        });
+        const varientsResult = await Promise.all(promises);
+        return { ...data, varients: varientsResult };
     },
-    /// Trigger build
 }));
--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -6,13 +6,31 @@ import { factories } from '@strapi/strapi'
 
 
 export default factories.createCoreController('api::product.product', ({ strapi }) => ({
-    // Method 1: Creating an entirely custom action
-    async exampleAction(ctx) {
-        return {
-            data: "Sucess"
-        }
-    },
 
-    /// Trigger build
+
+    async findOne(ctx) {
+
+        const orderid = ctx.params.id;
+
+
+        const data = await strapi.entityService.findOne('api::product.product', orderid, {
+            populate: ["images", "varients", "brands"],
+        });
+
+        const varients = data.varients;
+
+        let promises = [];
+
+        varients.forEach(({ id }) => {
+            promises.push(strapi.entityService.findOne('api::varient.varient', id, {
+                populate: ["images"],
+            }));
+        });
+
+
+        const varientsResult = await Promise.all(promises);
+
+        return { ...data, varients: varientsResult };
+    },
 
 }));
