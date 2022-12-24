f3f2bbb73fdd1ceb525bb0a2fc7f9c23a880f17f
dara <lykeosovandara@gmail.com>
2022-12-24 09:58:16 +0700

    update response

--- src/api/discount-config/controllers/discount-config.ts
+++ src/api/discount-config/controllers/discount-config.ts
@@ -4,4 +4,18 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::discount-config.discount-config');
+export default factories.createCoreController('api::discount-config.discount-config', ({ strapi }) => ({
+
+    async find(ctx) {
+
+        const discountes = await strapi.entityService.findMany('api::discount-config.discount-config', {
+            limit: 3,
+            sort: { createdAt: 'desc' },
+        })
+
+        return discountes;
+
+
+    }
+
+}));
